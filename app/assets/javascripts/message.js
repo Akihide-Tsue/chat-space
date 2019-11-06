72c31703cbc5389839654c14831317035ce02fe6
Akihide Tsue <akihide.tsue@gmail.com>
2019-11-06 14:19:08 +0900

    Nov_6_Jidoukoushin

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,13 +1,13 @@
 $(function(){
   function buildMessage(message){
-    image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : "";
-    var html =`<div class="message">
+  var image = message.image? `<img class= "lower-message__image" src=${message.image} >` : "";
+    var html =`<div class="message" data-message-id="${message.id}">
                 <div class="upper-message">
                   <div class="upper-message__user-name">
                     ${message.user_name}
                   </div>
                   <div class="upper-message__date">
-                    ${message.date}
+                    ${message.created_at}
                   </div>
                 </div>
                 <div class="lower-message">
@@ -20,10 +20,38 @@ $(function(){
     return html;
   }
 
+  var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    var last_message_id = $('.message:last').data('message-id');
+    $.ajax({
+      url: "api/messages",
+      type: "get",
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      console.log(messages)
+      var html = buildMessage(messages);//追加するHTMLの入れ物
+        messages.forEach(function (messages) {//配列messagesの中身をHTMLに変換し→入れ物に
+        html = buildMessage(messages); //メッセージが入ったHTMLを取得
+        $('.messages').append(html);//append
+      })
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('.form__submit').prop('disabled', false);
+      $('form')[0].reset();
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
+    })
+    }
+  }
+  setInterval(reloadMessages, 5000);
+
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
     var url = $(this).attr('action')
+    console.log("OK")
     $.ajax({
       url: url,  //同期通信でいう『パス』
       type: "POST",  //同期通信でいう『HTTPメソッド』
@@ -34,7 +62,7 @@ $(function(){
     })
     .done(function(message){
       var html = buildMessage(message);
-      $('.message').append(html);
+      $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $('.form__submit').prop('disabled', false);
       $('form')[0].reset();
@@ -43,4 +71,4 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     })
   })
-});
+})
