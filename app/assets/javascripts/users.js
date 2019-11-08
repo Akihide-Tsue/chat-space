53a05c123fbb3b6e58894b65b623b016e5ad0951
Akihide Tsue <akihide.tsue@gmail.com>
2019-11-08 16:18:47 +0900

    最新

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -55,7 +55,7 @@ $(function() {
         alert("通信エラーです。ユーザーが表示できません。");
       });
   });
-  $(document).on("click", ".chat-group-user__btn--add", function() {
+  $("#user-search-result").on("click", ".chat-group-user__btn--add", function() {
     console.log
     const userName = $(this).attr("data-user-name");
     const userId = $(this).attr("data-user-id");
@@ -65,7 +65,7 @@ $(function() {
     addDeleteUser(userName, userId);
     addMember(userId);
   });
-  $(document).on("click", ".chat-group-user__btn--remove", function() {
+  $(".js-add-user").on("click", ".chat-group-user__btn--remove", function() {
     $(this)
       .parent()
       .remove();
